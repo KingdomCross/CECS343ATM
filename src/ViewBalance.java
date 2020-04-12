424ab68d936d07c03932c3e2e674a37ea58090d3
KingdomCross <alexchheng1998@yahoo.com>
2020-04-12 04:30:20 -0700

    Balance variable getBalance toString Balance

--- src/ViewBalance.java
+++ src/ViewBalance.java
@@ -1,5 +1,20 @@
 //Alex Chheng
 public class ViewBalance {
+    double balance; //Account total variable
+
     public ViewBalance() { //Constructor
+        balance = 0;
+    }
+
+    public double getBalance(double n){
+        balance  = n;
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "ViewBalance{" +
+                "balance=" + balance +
+                '}';
     }
 }
